dbe47989bc4ba55107bfd77379f56e460e9a9d6c
Alex <alexcabaang@gmail.com>
2017-07-30 04:05:37 +0800

    Done with other services

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 // MODULE
-var angularApp = angular.module('angularApp', []);
+var angularApp = angular.module('angularApp', ['ngMessages', 'ngResource']);
 
 // CONTROLLERS
-angularApp.controller('mainController', function ($scope) {
-    $scope.name = 'Jane Doe';
-    $scope.occupation = 'Coder';
-    $scope.getname = function() {
-        return 'John Doe';
-    }
-    console.log($scope);
+angularApp.controller('mainController', function ($scope, $log, $filter, $resource) {
+
+    //    console.log($scope);
+    //    console.log($log);
+    //
+    //    $log.log("Hello");
+    //    $log.warn("Warning");
+    //    $log.info("This is an info...");
+    //    $log.error("Error...");
+
+    //    $scope.name = 'Alex';
+    //    $scope.formattedname = $filter('uppercase')($scope.name);
+    //
+    //    $log.info($scope.name);
+    //    $log.info($scope.formattedname);
+      
+    console.log($resource);
 });
-
-
-
