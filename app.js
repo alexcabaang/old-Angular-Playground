d3a6d8cde47f726003c5b181a31d4f4974eae78a
Alex <alexcabaang@gmail.com>
2017-07-30 15:05:49 +0800

    Base Code

--- app.js
+++ app.js
@@ -3,12 +3,7 @@ var angularApp = angular.module('angularApp', []);
 
 // CONTROLLERS
 angularApp.controller('mainController', function ($scope) {
-    $scope.name = 'Jane Doe';
-    $scope.occupation = 'Coder';
-    $scope.getname = function() {
-        return 'John Doe';
-    }
-    console.log($scope);
+    
 });
 
 
