6882890626680597b90d45506707c8bf4399b68d
Alex <alexcabaang@gmail.com>
2017-07-30 14:18:05 +0800

    Okay

--- app.js
+++ app.js
@@ -2,14 +2,12 @@
 var angularApp = angular.module('angularApp', []);
 
 // CONTROLLERS
-angularApp.controller('mainController', function ($scope) {
-    $scope.name = 'Jane Doe';
-    $scope.occupation = 'Coder';
-    $scope.getname = function() {
-        return 'John Doe';
+angularApp.controller('mainController', ['$scope', '$filter', function ($scope, $filter) {
+    $scope.handle = '';
+    $scope.lowercasehandle = function() {
+        return $filter('lowercase')($scope.handle); 
     }
-    console.log($scope);
-});
+}]);
 
 
 
